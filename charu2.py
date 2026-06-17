bc1068566037a33f1f4c23bdff3ecda9fef0772e
charuboopathi-BCA <charuboopathi222025@gmail.com>
2026-06-17 19:13:48 +0530

    F string + variable chain
    No error occurred on first try

---
+++ charu2.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+c=a+b
+d=b*c
+e=d/c
+f= e%d
+print (f" while a and b is added equal to c is {a+b}")
+print( f" while b and c is multipled and that is equal to d is {b*c}") 
+print ( f" while d and c is divided and that is equal to e is {d/c}")
+print ( f" while e and d is exponented and that is equal to f is { e%d}")
