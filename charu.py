d97c08f3df00792a8e76a86d707328155a100cbf
charuboopathi-BCA <charuboopathi222025@gmail.com>
2026-06-17 11:20:24 +0530

    Fixed syntax error. Learned variables + f string
    Corrected print f into print (f "... ")

---
+++ charu.py
@@ -0,0 +1,8 @@
+course=" BCA 2nd year"
+mission=" secure good job"
+day=1
+dream=" peaceful life while researching space in ISRO"
+print(f"day{day}:starts coding")
+print(f"status:{course} student on{mission}")
+print(f"student want{dream}")
+print (" no tools.only phone.no excuses")
