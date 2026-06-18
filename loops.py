8bd454ce874d73d5a47abb973d4fb70d1cd298e1
charuboopathi-BCA <charuboopathi222025@gmail.com>
2026-06-18 18:37:38 +0530

    Loops
    Got error and fixed it

---
+++ loops.py
@@ -0,0 +1,5 @@
+name= input ( "enter your coder name:")
+print(f"yo!{ name}, here is the power i am giving as orbit bot")
+for i in range (5) :
+    print(f"{i+1}.{name} is the dangerous person according to orbit bot😏")
+print(" loop completed,orbit bot signing off")
