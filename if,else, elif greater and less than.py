cf8ad02107cb4ca0c859210244bcac8df1667597
charuboopathi-BCA <charuboopathi222025@gmail.com>
2026-06-18 18:21:20 +0530

    Greater and lesser than and equal
    Orbit not talk to user and no error✌

---
+++ if,else, elif greater and less than.py
@@ -0,0 +1,10 @@
+print (" yo! I'm orbit bot,enter two numbers broo")
+a=int(input ( " enter the first number:"))
+b=int( input( " enter the second number:"))
+if a<b:
+    print(f" {a} is less than {b} broo")
+elif  a>b:
+    print( f" {a} greater than {b} broo")
+else:
+    print(f" {a} and {b} are equal broo")
+print( " yo! yo! orbit bot is signing off okay okay bye bye 👐")
